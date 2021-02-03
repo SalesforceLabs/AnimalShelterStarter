fbae865225d7769b1aea6a667427d1c77725b87f
Stewart Anderson <stewart.anderson@live.co.uk>
2021-02-03 10:12:25 +0000

    Updated Namespace References
    Updated all animalshelter__ references to %%%NAMESPACED_ORG%%%

--- force-app/main/default/aura/AnimalCloneDisplayFlowScreen/AnimalCloneDisplayFlowScreenController.js
+++ force-app/main/default/aura/AnimalCloneDisplayFlowScreen/AnimalCloneDisplayFlowScreenController.js
@@ -12,17 +12,17 @@
 ({
 	init  : function(component, event, helper) {
         component.set('v.columns',[
-            {label:'Animal Name', fieldName: 'animalshelter__Animal_Name__c',type:'text', editable: 'true'},
-            {label:'Primary Colour', fieldName: 'animalshelter__Colour__c',type:'text', editable: 'true'},
-            {label:'Secondary Colour', fieldName: 'animalshelter__Secondary_Colour__c',type:'text', editable: 'true'},
-            {label:'Gender', fieldName: 'animalshelter__Gender__c',type:'text', editable: 'true'},
-            {label:'Date of Birth', fieldName: 'animalshelter__Date_of_Birth__c',type:'date', editable: 'true'},
-            {label:'Crossbreed', fieldName: 'animalshelter__Crossbreed__c',type:'boolean', editable: 'true'},
-            {label:'Coat Type', fieldName: 'animalshelter__Coat_Type__c',type:'text', editable: 'true'},
-            {label:'Microchip or Tag', fieldName: 'animalshelter__Microchip__c',type:'text', editable: 'true'},
-            {label:'Size', fieldName: 'animalshelter__Size__c',type:'text', editable: 'true'},
-            {label:'Neutered/Spayed', fieldName: 'animalshelter__Neutered_Spayed__c',type:'boolean', editable: 'true'},
-            {label:'Neutered/Spayed Date/Time', fieldName: 'animalshelter__Neutered_Spayed_Date_Time__c',type:'date', editable: 'true'},
+            {label:'Animal Name', fieldName: '%%%NAMESPACED_ORG%%%Animal_Name__c',type:'text', editable: 'true'},
+            {label:'Primary Colour', fieldName: '%%%NAMESPACED_ORG%%%Colour__c',type:'text', editable: 'true'},
+            {label:'Secondary Colour', fieldName: '%%%NAMESPACED_ORG%%%Secondary_Colour__c',type:'text', editable: 'true'},
+            {label:'Gender', fieldName: '%%%NAMESPACED_ORG%%%Gender__c',type:'text', editable: 'true'},
+            {label:'Date of Birth', fieldName: '%%%NAMESPACED_ORG%%%Date_of_Birth__c',type:'date', editable: 'true'},
+            {label:'Crossbreed', fieldName: '%%%NAMESPACED_ORG%%%Crossbreed__c',type:'boolean', editable: 'true'},
+            {label:'Coat Type', fieldName: '%%%NAMESPACED_ORG%%%Coat_Type__c',type:'text', editable: 'true'},
+            {label:'Microchip or Tag', fieldName: '%%%NAMESPACED_ORG%%%Microchip__c',type:'text', editable: 'true'},
+            {label:'Size', fieldName: '%%%NAMESPACED_ORG%%%Size__c',type:'text', editable: 'true'},
+            {label:'Neutered/Spayed', fieldName: '%%%NAMESPACED_ORG%%%Neutered_Spayed__c',type:'boolean', editable: 'true'},
+            {label:'Neutered/Spayed Date/Time', fieldName: '%%%NAMESPACED_ORG%%%Neutered_Spayed_Date_Time__c',type:'date', editable: 'true'},
         ]);
     },
     updateSelectedAnimals : function(component, event, helper){

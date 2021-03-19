c6163a48a5b063eac84794c4726e75f9164262e0
Chris Rolfe <cjrolfe@icloud.com>
2021-03-19 15:13:03 +0000

    Changed to Aura Component

---
+++ force-app/main/default/aura/FSCGeolocation/FSCGeolocationController.js
@@ -0,0 +1,6 @@
+({
+    init : function(component, event, helper){
+        helper.getLocation(componet, event);
+        console.log('Init Called');
+    }
+})
---
+++ force-app/main/default/aura/FSCGeolocation/FSCGeolocationHelper.js
@@ -0,0 +1,16 @@
+({
+    getLocation : function(component,event){
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success);
+            function success(position){
+                var currLat = position.coords.latitude;
+                component.set("v.latitude", currLat);
+                var currLong = position.coords.longitude;
+                component.set("v.longitude", currLong);
+                console.log("CurrentLocation: " + currLat + "," + currLong);
+            }
+        } else {
+            error('Geolocation Not available, check device settings');
+        }
+    }
+})

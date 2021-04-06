d633de1971005e47373d1178dc9678bc3ec430d1
Chris Rolfe <cjrolfe@icloud.com>
2021-04-06 09:42:06 +0100

    Updates to FSCGeolocation

--- force-app/main/default/aura/FSCGeolocation/FSCGeolocationController.js
+++ force-app/main/default/aura/FSCGeolocation/FSCGeolocationController.js
@@ -1,6 +1,22 @@
 ({
-    init : function(component, event, helper){
-        helper.getLocation(component, event);
-        console.log('Init Called');
+    
+    init : function(component, event, helper) {
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success);
+            function success(position){
+                var currLat = position.coords.latitude;
+                component.set("v.latitude", currLat);
+                var currLong = position.coords.longitude;
+                component.set("v.longitude", currLong);
+                console.log("CurrentLocation: " + currLat + "," + currLong);
+                
+                var marker = "";
+                var marker = '[{"location": { "Latitude": "' + currLat + '","Longitude": "' + currLong + '"}}]';
+                console.log("Marker Data: " + marker);
+                component.set("v.mapMarkersData", JSON.parse(marker));
+            }
+        } else {
+            error('Geolocation Not available, check device settings');
+        }
     }
 })
--- force-app/main/default/aura/FSCGeolocation/FSCGeolocationHelper.js
+++ force-app/main/default/aura/FSCGeolocation/FSCGeolocationHelper.js
@@ -1,16 +1,5 @@
 ({
-    getLocation : function(component, event){
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(success);
-            function success(position){
-                var currLat = position.coords.latitude;
-                component.set("v.latitude", currLat);
-                var currLong = position.coords.longitude;
-                component.set("v.longitude", currLong);
-                console.log("CurrentLocation: " + currLat + "," + currLong);
-            }
-        } else {
-            error('Geolocation Not available, check device settings');
-        }
+    helperMethod : function(){
+        
     }
 })

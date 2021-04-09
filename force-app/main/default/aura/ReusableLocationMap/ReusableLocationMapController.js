c6e65a11ddf180882661b26345c070939d9d1e23
Chris Rolfe <cjrolfe@icloud.com>
2021-04-09 14:09:53 +0100

    Added Reusable Map Component

---
+++ force-app/main/default/aura/ReusableLocationMap/ReusableLocationMapController.js
@@ -0,0 +1,28 @@
+({
+    init : function(component, event, helper){
+        var action = component.get("c.getLocation");
+        action.setParams({
+            varObj: component.get("v.obj"),
+            varLat: component.get("v.latField"),
+            varLong: component.get("v.longField"),
+            varName: component.get("v.nameField"),
+            varSelect: component.get("v.selectField"),
+            varDescText: component.get("v.descTextField"),
+            varDescDate: component.get("v.descDateField")
+        });
+        action.setCallback(this, function(response){
+            console.log('response'+response);
+            var state = response.getState();
+            console.log(state);
+            if (state == 'SUCCESS'){
+                var obj = response.getReturnValue();
+                console.log(obj);
+                component.set('v.mapMarkers', obj);
+                component.set('v.zoomLevel', 16);
+                component.set('v.markersTitle', 'Animal Locations');
+                component.set('v.showFooter', false);
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})

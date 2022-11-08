bec3893fd0d09aad91e57550d7528cab67328957
Stewart Anderson <stewart.anderson@salesforce.com>
2022-11-08 10:55:22 +0000

    Update AddNameSpace.py

--- scripts/AddNameSpace.py
+++ scripts/AddNameSpace.py
@@ -5,31 +5,13 @@
 import glob
 import re
 from os.path import exists
+from typing import Optional
 
 NAMESPACE_REPLACEMENT = f"%%%NAMESPACED_ORG%%%"
 USE_NAMESPACE_REPLACEMENT = True
 SEARCH_FOLDER = "force-app/main/default"
 
-def replace_file_text(file_location, old_text, new_text):
-    """ Replace specific text within a file
-    :param file_location: Relative path and file name of the file you want to replace text within
-    :param old_text: Text string to search for
-    :param new_text: New text to replace old text
-    """
-    if not exists(file_location):
-        raise Exception(f"Error: File Path does not exist. Check the file {file_location}")
-
-    with open(f"{file_location}", "r") as tmpFile:
-        tmpFile.seek(0)
-        fcontents = tmpFile.read()
-
-
-    new_fcontents = fcontents.replace(f"{old_text}", f"{new_text}")
-
-    with open(f"{file_location}", "w") as tmpFile:
-        tmpFile.write(new_fcontents)
-
-def update_files():
+def update_files(RemoveOnlyMode: Optional[bool] = False):
   
   all_files = []
   aura_files = glob.glob(SEARCH_FOLDER + "/**/*.cmp", recursive=True)
@@ -46,37 +28,25 @@ def update_files():
   all_files.extend(vf_files)
   all_files.extend(m_rule_files)
 
+  # Process Each File Found
   for file in all_files:
 
-    replace_file_text(file, NAMESPACE_REPLACEMENT, "")
-
-    # Search for all references within file
+    # Search for all custom references within file
+    print(f"\n**Processing File**: {file}")
     with open(file, "r") as temp_file:
       temp_file.seek(0)
       temp_file_string = temp_file.read()
 
-      items_to_replace = []
-      custom_references = re.findall("[\w]+__c", temp_file_string)
-      lookup_references = re.findall("[\w]+__r", temp_file_string)
-      gps_references = re.findall("[\w]+__s", temp_file_string)
+    # Update File String with new References
+    temp_file_string = temp_file_string.replace(NAMESPACE_REPLACEMENT, "")
 
-      items_to_replace.extend(custom_references)
-      items_to_replace.extend(lookup_references)
-      items_to_replace.extend(gps_references)
+    if not RemoveOnlyMode:
+      temp_file_string = re.sub("[\w]+__c", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
+      temp_file_string = re.sub("[\w]+__r", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
+      temp_file_string = re.sub("[\w]+__s", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
 
-    print(f"\n**Processing File**: {file}")
+    with open(file, "w") as tmpFile:
+          tmpFile.write(temp_file_string)
 
-    # De-dupe List
-    original_count = len(items_to_replace)
-    items_to_replace = list(dict.fromkeys(items_to_replace))
-
-    if len(items_to_replace) > 0:
-      print(f"    {len(items_to_replace)} Unique References Found ({original_count} Total References in file)")
-      for item in items_to_replace:
-        replace_file_text(file, item, f"{NAMESPACE_REPLACEMENT}{item}")
-        print(f"      Updated Reference: {item} to {NAMESPACE_REPLACEMENT}{item}")
-    else:
-      print(f"      Skipping File: {file} as no custom references were found.")
-
-
+# Run Task
 update_files()

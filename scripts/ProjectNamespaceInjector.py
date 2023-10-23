cba0f87e4d7b35c3b6ccabf3f2bcd01311265fb9
Stewart Anderson <stewart.anderson@salesforce.com>
2023-10-23 11:20:27 +0100

    Updates
    - Added Namespace Placeholder
    - Removed standard object page layouts for task and event.

--- scripts/ProjectNamespaceInjector.py
+++ scripts/ProjectNamespaceInjector.py
@@ -8,10 +8,10 @@
 # 3. Done
 #
 
-import glob
-import re
 import fileinput
+import glob
 import os
+import re
 
 NAMESPACE_REPLACEMENT = "%%%NAMESPACED_ORG%%%"
 SEARCH_FOLDER = "force-app/main/default"
@@ -23,11 +23,15 @@ FILE_PATTERNS = [
     "/pages/*.page-meta.xml",
     "/**/*.matchingRule-meta.xml",
     "/quickActions/*.quickAction-meta.xml",
-    "/lwc/**/*.js-meta.xml"
+    "/lwc/**/*.js-meta.xml",
 ]
 
-def update_files(namespace_placeholder: str = None, search_directory: str = None, remove_prefix_mode: bool = False):
-    
+
+def update_files(
+    namespace_placeholder: str = None,
+    search_directory: str = None,
+    remove_prefix_mode: bool = False,
+):
     """
     Updates the custom object references throughout the files held within the search directory which is assumed to have a default Salesforce DX structure.
 
@@ -40,35 +44,54 @@ def update_files(namespace_placeholder: str = None, search_directory: str = None
 
     # Process initial Checks
     if not namespace_placeholder:
-      namespace_placeholder = input(f"Please confirm the placeholder which will be replaced, the default is '{NAMESPACE_REPLACEMENT}': ") or NAMESPACE_REPLACEMENT
-    
-    if not search_directory:
-      search_directory = input(f"Please confirm the search directory path which you want to search within, the default is {SEARCH_FOLDER}: ") or SEARCH_FOLDER
+        namespace_placeholder = (
+            input(
+                f"Please confirm the placeholder which will be replaced, the default is '{NAMESPACE_REPLACEMENT}': "
+            )
+            or NAMESPACE_REPLACEMENT
+        )
 
-    remove_prefix_confirmation = input("Do you want to only remove the current prefixes (if found)? Y/n ") or 'y'
-    remove_prefix_mode = True if remove_prefix_confirmation.lower() == 'y' else False
+    if not search_directory:
+        search_directory = (
+            input(
+                f"Please confirm the search directory path which you want to search within, the default is {SEARCH_FOLDER}: "
+            )
+            or SEARCH_FOLDER
+        )
+
+    remove_prefix_confirmation = (
+        input("Do you want to only remove the current prefixes (if found)? Y/n ") or "y"
+    )
+    remove_prefix_mode = True if remove_prefix_confirmation.lower() == "y" else False
 
     if not os.path.exists(search_directory):
-      print("Error: The file path defined does not exist. Please update the file path and try again.")
-      return
-    
+        print(
+            "Error: The file path defined does not exist. Please update the file path and try again."
+        )
+        return
+
     # Locate all relevant files
     all_files = []
-    
+
     for pattern in FILE_PATTERNS:
         all_files.extend(glob.glob(SEARCH_FOLDER + pattern, recursive=True))
-    
+
     # Process Each File Found
     for file in all_files:
         print(f"\n**Processing File**: {file}")
-        with fileinput.input(files=(file,), inplace=True, encoding="utf-8") as file_to_check:
+        with fileinput.input(
+            files=(file,), inplace=True, encoding="utf-8"
+        ) as file_to_check:
             for line in file_to_check:
                 updated_line = line.replace(NAMESPACE_REPLACEMENT, "")
 
                 if not remove_prefix_mode:
-                  updated_line = re.sub("[\w]+__[crs]", f"{NAMESPACE_REPLACEMENT}\g<0>", updated_line)
+                    updated_line = re.sub(
+                        "[\w]+__[crs]", f"{NAMESPACE_REPLACEMENT}\g<0>", updated_line
+                    )
 
                 print(updated_line, end="")
+
 
 # Run Task
 if __name__ == "__main__":

33d4363bf371b6edac5662547e276988eb156335
Stewart Anderson <stewart.anderson@salesforce.com>
2023-10-24 10:39:29 +0100

    Additional Fixes

--- force-app/main/default/aura/AlertBanner/AlertBannerController.js
+++ force-app/main/default/aura/AlertBanner/AlertBannerController.js
@@ -1,41 +1,41 @@
 /**
  * @File Name          : AlertBannerController.js
- * @Description        : 
+ * @Description        :
  * @Author             : Stewart Anderson (Salesforce.org)
- * @Group              : 
- * @Last Modified By   : Stewart Anderson (Salesforce.org)
- * @Last Modified On   : 4/7/2020, 8:57:28 AM
- * @Modification Log   : 
+ * @Group              :
+ * @Last Modified By   : Stewart Anderson
+ * @Last Modified On   : 10-23-2023
+ * @Modification Log   :
  * Ver       Date            Author      		    Modification
  * 1.0    4/7/2020   Stewart Anderson (Salesforce.org)     Initial Version
 **/
 ({
-	doInit: function(component, event, helper) { 
-        
+    doInit: function (component, event, helper) {
+
         //Get Animal Alert Records
         var action = component.get("c.getRelatedAlertRecords");
         action.setParams({
             parentId: component.get("v.recordId"),
         });
-        
-        action.setCallback(this, function(response) {
-            
+
+        action.setCallback(this, function (response) {
+
             //Handle Response
             if (response.getState() === "SUCCESS") {
-                
+
                 var AnimalBoxDiv = component.find("AlertBox");
-                var AnimalAlertList = response.getReturnValue(); 
-                component.set("v.intAnimalAlertsSize",AnimalAlertList.length);
-                
-                if(AnimalAlertList.length > 0){
-                    component.set("v.lstAnimalAlerts",response.getReturnValue());
+                var AnimalAlertList = response.getReturnValue();
+                component.set("v.intAnimalAlertsSize", AnimalAlertList.length);
+
+                if (AnimalAlertList.length > 0) {
+                    component.set("v.lstAnimalAlerts", response.getReturnValue());
                     $A.util.removeClass(component.find("AlertBox"), "slds-hide");
                     $A.util.addClass(component.find("AlertBox"), "AnimalAlertClass");
                 }
-               
-            } 
+
+            }
             else if (state == "ERROR") {
-                
+
                 //Handle Error
                 var toastEvent = $A.get("e.force:showToast");
                 toastEvent.setParams({
@@ -43,9 +43,9 @@
                     "message": "Please contact your administrator, there was an error retrieving animal alerts related to this record."
                 });
                 toastEvent.fire();
-            }    
+            }
         });
-        
+
         $A.enqueueAction(action);
     }
 })
--- force-app/main/default/aura/AnimalActionRelatedList2/AnimalActionRelatedList2Helper.js
+++ force-app/main/default/aura/AnimalActionRelatedList2/AnimalActionRelatedList2Helper.js
@@ -1,25 +1,32 @@
+/**
+ * @description       :
+ * @author            : Stewart Anderson
+ * @group             :
+ * @last modified on  : 10-23-2023
+ * @last modified by  : Stewart Anderson
+**/
 ({
-	getData : function(cmp) {
+    getData: function (cmp) {
         var action = cmp.get('c.getAnimalActions');
         action.setParams({
             recId: cmp.get("v.recordId"),
             rTypeName: cmp.get("v.recordTypeName")
         });
-        action.setCallback(this, $A.getCallback(function (response){
+        action.setCallback(this, $A.getCallback(function (response) {
             var state = response.getState();
             if (state === "SUCCESS") {
                 //cmp.set("v.mydata", response.getReturnValue());
-                var records =response.getReturnValue();
-                records.forEach(function(record){
-                    record.linkName = '/'+record.Id;
-                    if(record.%%%NAMESPACED_ORG%%%Action_Completed__c){
-                            record.showClass = (record.%%%NAMESPACED_ORG%%%Action_Completed__c === true ? 'redcolor' : 'blackcolor');
-                            record.displayIconName = 'utility:check';  
-                        }
-                        else{
-                            record.showClass = (record.%%%NAMESPACED_ORG%%%Action_Completed__c === false ? 'blackcolor' : 'redcolor');
-                            record.displayIconName = 'utility:close';     
-                        }
+                var records = response.getReturnValue();
+                records.forEach(function (record) {
+                    record.linkName = '/' + record.Id;
+                    if (record.%%%NAMESPACED_ORG%%%Action_Completed__c) {
+                        record.showClass = (record.%%%NAMESPACED_ORG%%%Action_Completed__c === true ? 'redcolor' : 'blackcolor');
+                        record.displayIconName = 'utility:check';
+                    }
+                    else {
+                        record.showClass = (record.%%%NAMESPACED_ORG%%%Action_Completed__c === false ? 'blackcolor' : 'redcolor');
+                        record.displayIconName = 'utility:close';
+                    }
                 });
                 cmp.set("v.mydata", records);
                 cmp.set("v.mydataLength", response.getReturnValue().length);
@@ -29,6 +36,6 @@
             }
         }));
         $A.enqueueAction(action);
-		
-	}
+
+    }
 })

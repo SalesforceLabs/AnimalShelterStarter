ba3d31d55d764a2355fa898eacbe37d12df02f9d
Stewart Anderson <stewart.anderson@salesforce.com>
2023-11-06 15:56:49 +0000

    Added Badges

--- force-app/main/default/aura/lightningPrintViewApp/lightningPrintViewApp.app
+++ force-app/main/default/aura/lightningPrintViewApp/lightningPrintViewApp.app
@@ -2,6 +2,7 @@
   <aura:attribute name="recordid" type="String" />
   <aura:attribute name="fieldSetName" type="String" />
   <aura:attribute name="sObjectTypeName" type="String" />
+  <aura:attribute name="badgefields" type="String" />
 
   <div class="slds-clearfix slds-p-around_medium">
     <div class="slds-float_right">
@@ -9,7 +10,7 @@
     </div>
   </div>
 
-  <c:lightningPrintView recordId="{!v.recordid}" fieldSetName="{!v.fieldSetName}"
-    sObjectTypeName="{!v.sObjectTypeName}" />
+  <c:lightningPrintView recordId="{!v.recordid}" fieldSetName="{!v.fieldSetName}" sObjectTypeName="{!v.sObjectTypeName}"
+    badgefields="{!v.badgefields}" />
 
 </aura:application>
--- force-app/main/default/lwc/lightningPrintView/lightningPrintView.js
+++ force-app/main/default/lwc/lightningPrintView/lightningPrintView.js
@@ -5,14 +5,18 @@
  * @last modified on  : 11-06-2023
  * @last modified by  : Stewart Anderson
 **/
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import getFieldSet from '@salesforce/apex/AnimalShelterFieldsetController.getFieldSet';
+import getBadgeFieldsData from '@salesforce/apex/AnimalShelterFieldsetController.getBadgeFieldsData';
 
 export default class LightningPrintPreview extends LightningElement {
   @api recordId;
   @api fieldSetName;
   @api sObjectTypeName;
+  @api badgefields;
+  @track badgeData = {};
+
 
   fieldSetMembers = [];
   recordData;
@@ -50,8 +54,8 @@ export default class LightningPrintPreview extends LightningElement {
     if (data) {
       this.recordData = data;
       this.error = undefined;
-      console.log('Data returned')
-      console.log(JSON.stringify(data))
+      //console.log('Data returned')
+      //console.log(JSON.stringify(data))
     } else if (error) {
       this.error = 'Error fetching record data.';
       console.error('Record Data Error:', error);
@@ -78,4 +82,28 @@ export default class LightningPrintPreview extends LightningElement {
       };
     });
   }
+
+  @wire(getBadgeFieldsData, {
+    recordId: '$recordId',
+    sObjectName: '$sObjectTypeName',
+    badgeFieldNames: '$parsedBadgeFields'
+  })
+  wiredBadgeData({ error, data }) {
+    if (data) {
+      // Flatten the values into a single list of badges
+      let badges = [];
+      for (const [key, value] of Object.entries(data)) {
+        badges = badges.concat(value);
+      }
+      this.badgeData = badges;
+    } else if (error) {
+      console.error('Error retrieving badge data:', error);
+      // Handle error appropriately
+    }
+  }
+
+  get parsedBadgeFields() {
+    return this.badgefields ? this.badgefields.split(',') : [];
+  }
+
 }

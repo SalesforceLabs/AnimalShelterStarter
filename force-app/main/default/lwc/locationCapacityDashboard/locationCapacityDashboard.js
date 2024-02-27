0752bc4291f60a40b9e24d3e9be346d1d985ebb5
Chris Rolfe <cjrolfe@icloud.com>
2024-02-27 14:21:31 +0000

    Update to include Site Filter

--- force-app/main/default/lwc/locationCapacityDashboard/locationCapacityDashboard.js
+++ force-app/main/default/lwc/locationCapacityDashboard/locationCapacityDashboard.js
@@ -1,9 +1,11 @@
 import { LightningElement, wire, track } from 'lwc';
 import getLocations from '@salesforce/apex/LocationCapacityDashboardController.getLocations';
+import getSites from '@salesforce/apex/LocationCapacityDashboardController.getSites';
 
 export default class LocationCapacityDashboard extends LightningElement {
 
     @track locations = [];
+    @track siteOptions = [];
     @track error;
     selectedSiteId;
 
@@ -15,6 +17,19 @@ export default class LocationCapacityDashboard extends LightningElement {
         } else if (error) {
             this.error = error;
             this.locations = undefined;
+        }
+    }
+    @wire(getSites)
+    wiredSites({ error, data }){
+        if (data) {
+            this.siteOptions = data.map(site => ({
+                label: site.animalshelters__Name__c,
+                value: site.siteId
+            }));
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.siteOptions = undefined;
         }
     }
 

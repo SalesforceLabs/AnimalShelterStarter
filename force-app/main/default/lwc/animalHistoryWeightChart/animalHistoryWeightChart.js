b8ff0e55c37d9cffde38d4a85c5f214f9077c9ef
Chris Rolfe <cjrolfe@icloud.com>
2024-03-15 13:28:31 +0000

    Added LWC

---
+++ force-app/main/default/lwc/animalHistoryWeightChart/animalHistoryWeightChart.js
@@ -0,0 +1,67 @@
+/**
+ * @description       : Displays a Chart of Animal Current Weight changes
+ * @author            : Chris Rolfe
+ * @group             :
+ * @last modified on  : 03-15-2023
+ * @last modified by  : Chris Rolfe
+**/
+import { LightningElement, api, wire, track } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import chartjs from '@salesforce/resourceUrl/chartjs280';
+import getWeigthHistory from '@salesforce/apex/AnimalHistoryWeightChartController.getWeigthHistory';
+
+export default class AnimalHistoryWeightChart extends LightningElement {
+    @api recordId;
+    @track chartjsInitialized = false;
+    chart;
+
+    @wire(getWeightHistory, { recordId: '$recordId' })
+    weightHistory({ error, data }) {
+        if (data) {
+            console.log(JSON.stringify(data));
+            //let dates = JSON.parse(JSON.stringify(data)).map(record => record.cwDate);
+            //let weights = JSON.parse(JSON.stringify(data)).map(record => record.cwWeight);
+            let dates = data.map(record => record.cwDate);
+            let weights = data.map(record => record.cwWeight);
+            console.log('Dates:', dates);
+            console.log('Weights:', weights);
+            this.renderChart(dates, weights);
+        } else if (error) {
+            // Handle error
+            console.error('Error:', error);
+        }
+    }
+
+    renderedCallback() {
+        if (this.chartjsInitialized) {
+            return;
+        }
+        this.chartjsInitialized = true;
+
+        Promise.all([
+            loadScript(this, chartjs)])
+            .then(() => {
+                this.renderChart();
+            })
+            .catch(error => {
+                console.error('Error loading ChartJS', error);
+            });
+    }
+
+    renderChart(dates, weights) {
+        const ctx = this.template.querySelector('canvas').getContext('2d');
+        this.chart = new window.Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'Weight',
+                    data: weights,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            }
+        });
+    }
+}
